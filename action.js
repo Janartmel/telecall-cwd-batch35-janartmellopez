31dc437c4a337ba86d52ee4df0b70a28de37736f
Janartmel <lopez.janartmel.t@gmail.com>
2024-11-13 01:07:09 +0800

    Update action.js
    Signed-off-by: Janartmel <lopez.janartmel.t@gmail.com>

--- action.js
+++ action.js
@@ -5,7 +5,11 @@ links[0].addEventListener("click", () => { openlink(1) });
 links[1].addEventListener("click", () => { openlink(2) });
 links[2].addEventListener("click", () => { openlink(3) });
 links[3].addEventListener("click", () => { openlink(4) });
-
+links[4].addEventListener("click", () => { openlink(5) });
+links[5].addEventListener("click", () => { openlink(6) });
+links[6].addEventListener("click", () => { openlink(7) });
+links[7].addEventListener("click", () => { openlink(8) });
+links[8].addEventListener("click", () => { openlink(9) });
 
 function openlink(x) {
     if (x == 1) {
@@ -20,4 +24,18 @@ function openlink(x) {
     if (x == 4) {
         window.open("https://www.facebook.com/janartmellopez/", "_blank");
     }
+
+    if (x == 5) {
+        window.open("https://www.instagram.com/artmellopez/", "_blank");
+    }
+    if (x == 6) {
+        window.open("https://www.linkedin.com/in/jan-artmel-lopez-47b190232/", "_blank");
+    }
+    if (x == 7) {
+        window.open("https://github.com/Janartmel", "_blank");
+    }
+    if (x == 8) {
+        window.open("https://www.facebook.com/janartmellopez/", "_blank");
+    }
 }
+
